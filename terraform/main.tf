a47a678192ea6ccd3d49de9b1330496f9bbeac36
Shubhamtrip <shubhamtrip16@gmail.com>
2025-05-22 18:25:19 +0530

    added terraform

---
+++ terraform/main.tf
@@ -0,0 +1,45 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_key_pair" "finance_me_key" {
+  key_name   = "finance_me_key"
+  public_key = file("~/.ssh/id_rsa.pub")  # Your public key path
+}
+
+resource "aws_security_group" "finance_me_sg" {
+  name        = "finance_me_sg"
+  description = "Allow SSH and app traffic"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # For SSH access (restrict for production)
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # App port open for testing
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "finance_me_ec2" {
+  ami           = "ami-0c02fb55956c7d316"  
+  instance_type = "t2.micro"
+  key_name      = aws_key_pair.finance_me_key.key_name
+  security_groups = [aws_security_group.finance_me_sg.name]
+
+  tags = {
+    Name = "finance-me-app-server"
+  }
+}
