0a85d1a72151a1d2751f5c17f66150a5f75ab576
Shubhamtrip <shubhamtrip16@gmail.com>
2025-05-24 15:25:58 +0530

    secret

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,7 +1,11 @@
-
 output "test_server_ip" {
   description = "Public IP of the test server"
   value       = aws_instance.test_server.public_ip
+}
+
+output "test_server_instance_id" {
+  description = "Instance ID of the test server"
+  value       = aws_instance.test_server.id
 }
 
 output "prod_server_ip" {
